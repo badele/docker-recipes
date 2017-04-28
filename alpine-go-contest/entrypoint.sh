22c779e6afc418937f9c9deb9c125d6a96087777
badele <bruno@adele.im>
2017-04-28 13:03:00 +0200

    Update Entrypoint

--- alpine-go-contest/entrypoint.sh
+++ alpine-go-contest/entrypoint.sh
@@ -4,3 +4,5 @@ if [ ! -e /opt/go-contest/data/contest.sqlite3 ]; then
     echo "Create database"
     sqlite3 /opt/go-contest/data/contest.sqlite3 < /opt/go-contest/create_database.sql
 fi
+
+exec "$@"
